29e70c480649134597cbc4c3b1e649bba6a3e210
jongheecode <jonghee12386@gmail.com>
2026-01-11 12:51:29 +0900

    JVM 버전 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-	kotlin("jvm") version "2.2.21"
-	kotlin("plugin.spring") version "2.2.21"
-	id("org.springframework.boot") version "4.0.1"
-	id("io.spring.dependency-management") version "1.1.7"
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
 }
 
 group = "com.kpu"
 version = "0.0.1-SNAPSHOT"
-description = "KPU Capstone-Backend"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	implementation("software.amazon.awssdk:ec2:2.21.0")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("software.amazon.awssdk:ec2:2.21.0")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
-	}
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
--- src/main/kotlin/com/kpu/backend/BackendApplication.kt
+++ src/main/kotlin/com/kpu/backend/BackendApplication.kt
@@ -9,3 +9,5 @@ class BackendApplication
 fun main(args: Array<String>) {
 	runApplication<BackendApplication>(*args)
 }
+//curl -X POST "http://localhost:8080/api/ec2/create?userId=jonghee" - 맥북 터미널 테스트 명령어
+//Invoke-RestMethod -Method Post -Uri "http://localhost:8080/api/ec2/create?userId=jonghee" - 윈도우 파워쉘 테스트 명령어
